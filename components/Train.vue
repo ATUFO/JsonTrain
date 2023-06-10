a8bea14b84bce3b7486a879ccfbcf09942754eb8
陶福 <2221057667@qq.com>
2023-06-10 20:00:09 +0800

    init

---
+++ components/Train.vue
@@ -0,0 +1,105 @@
+<template>
+    <div style="width: 100%;">
+        <div style="width:100vw;background-color:#3883fa;color: aliceblue;position: absolute;left:0;top:0;padding: 10px;">
+            <span style="font-family: 'Lucida Sans';font-weight: bolder;font-size: x-large;">
+            JSON TRAIN
+            </span>
+        </div>
+        <div class="vje_container" style="margin-top:50px;">
+            <div class="vje_src">
+                <vue-json-editor style="height: 100%;" v-model="resourceJson" :showBtns="false" :mode="'code'"
+                    @json-change="onJsonChange" @json-save="onJsonSave" @has-error="onError" />
+            </div>
+            <div class="vje_ex" v-if="exJson != undefined">
+                <vue-json-editor height="100" style="height: 100%;" v-model="exJson" :showBtns="false" :mode="'code'"
+                    @json-change="onJsonChange" @json-save="onJsonSave" @has-error="onError" />
+            </div>
+        </div>
+        <br>
+        <div>
+            <el-tabs v-model="tab" type="card">
+                <el-tab-pane label="JsonPath" name="jsonpath">
+                    <el-input v-model="xpath" @input="onPathChange" type="text">
+                        <template slot="prepend">$</template>
+                    </el-input>
+                </el-tab-pane>
+                <el-tab-pane label="JS" name="js">
+                    <el-input v-model="jst" @input="onJstChange" type="text">
+                        <template slot="prepend">this.</template>
+                    </el-input>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
+    </div>
+</template>
+<script>
+import vueJsonEditor from 'vue-json-editor'
+import jp from "jsonpath"
+export default {
+    name: "Train",
+    components: { vueJsonEditor },
+    data() {
+        return {
+            tab:'jsonpath',
+            resourceJson: {},
+            xpath: "",
+            jst:"",
+            exJson: undefined
+        }
+    },
+    methods: {
+        onJsonSave() { },
+        onJsonChange(val) {
+            console.log(val);
+        },
+        onJstChange() {
+            console.log(this.xpath);
+            try {
+                var result = eval("this.resourceJson."+this.jst)
+                if(typeof(result) === 'function'){
+                    this.exJson = undefined
+                }else{
+                    this.exJson = result
+                }
+                // console.log(result);
+            } catch (error) {
+                this.exJson = undefined
+            }
+        },
+        onPathChange() {
+            console.log(this.xpath);
+
+            this.showEx = this.xpath !== ''
+            var p = "$" + this.xpath
+            try {
+                this.exJson = jp.query(this.resourceJson, p)
+            } catch (error) {
+                this.exJson = undefined
+            }
+        },
+        onError() { }
+    }
+
+}
+</script>
+<style>
+.vje_container {
+    padding: 0 10px;
+    display: flex;
+    width: 100%;
+    height: 70vh;
+    justify-content: space-around;
+    align-items: stretch;
+}
+
+.vje_ex,
+.vje_src {
+    width: 100%;
+    height: 100%;
+    margin: 10px;
+}
+
+.jsoneditor-vue {
+    height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Train></Train>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
